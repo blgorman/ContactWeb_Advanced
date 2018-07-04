86946eb4aac87b0c17ef72d56faf3d00bc3d6330
Brian L. Gorman <brian@farreachinc.com>
2018-07-04 13:06:40 -0500

    Added Emailer Library and sendgrid packages, reference to libraries project

---
+++ ContactWeb.Libraries/Emailer.cs
@@ -0,0 +1,21 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace ContactWeb.Libraries
+{
+    public class Emailer
+    {
+        public static async Task Execute(EmailInputDto input)
+        {
+            var apiKey = ConfigurationManager.AppSettings["ContactWebSendgridKey"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(input.FromAddress, input.FromFormalName);
+            var subject = input.Subject;
+            var to = new EmailAddress(input.ToAddress, input.ToFormalName);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, null, input.Body);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
